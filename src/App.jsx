6579faed71267a5d8321361ab6827a18cdaf2a75
Saurav kumar verma <sauravkumarverma22@gmail.com>
2024-01-26 03:29:55 +0530

    Making the Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
 import "./App.css";
-import Banner from "./components/Banner/Banner";
+import img from "./assets/images/image-omelette.jpeg";
 
 const App = () => {
   return (
     <div className="app">
-      <Banner />
-      <h2>Body</h2>
+      <div className="container">
+        <header className="header">
+          <img src={img} alt="banner img" />
+        </header>
+        <main>
+          <h1 className="primary-heading">Simple Omelette Recipe</h1>
+          <p className="text">
+            An easy and quick dish, perfect for any meal. This classic omelette
+            combines beaten eggs cooked to perfection, optionally filled with
+            your choice of cheese, vegetables, or meats.
+          </p>
+
+          <div className="box-1">
+            <h3 className="box-title">Preparation time</h3>
+            <ul className="box-list">
+              <li>
+                <strong>Total:</strong> Approximately 10 minutes
+              </li>
+              <li>
+                <strong>Preparation:</strong> 5 minutes
+              </li>
+              <li>
+                <strong>Cooking:</strong> 5 minutes
+              </li>
+            </ul>
+          </div>
+        </main>
+      </div>
     </div>
   );
 };
