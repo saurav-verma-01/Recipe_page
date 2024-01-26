cc2ccb35b176084dd5bd0e91d818f88303f5a892
Saurav kumar verma <sauravkumarverma22@gmail.com>
2024-01-26 15:12:03 +0530

    Working on Optimization

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+import { minifyHtml, injectHtml } from "vite-plugin-html";
+import { minify } from "terser";
+import cssnano from "cssnano";
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    minifyHtml(),
+    injectHtml({
+      injectData: {
+        injectCSS: '<link rel="stylesheet" href="/assets/index.css">',
+        injectJS: '<script defer src="/assets/index.js"></script>',
+      },
+    }),
+  ],
+  build: {
+    // Use minification for production build
+    minify: "terser",
+    rollupOptions: {
+      output: {
+        manualChunks: {},
+      },
+    },
+    // Additional configuration for CSS minification
+    cssCodeSplit: false,
+    cssMinify: cssnano(),
+  },
+});
